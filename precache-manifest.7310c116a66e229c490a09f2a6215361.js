1fbe86d7479f3135702d612f47f86c4923634c08
Islamova Akmaral <akmaralslmv@gmail.com>
2019-11-24 12:41:18 +0600

    Updates

---
+++ precache-manifest.7310c116a66e229c490a09f2a6215361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa33ef30ee1487faf4811f83bd922d0a",
+    "url": "/reactive-todo/index.html"
+  },
+  {
+    "revision": "6cf2e2971caa2795c4d1",
+    "url": "/reactive-todo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "30ac442b3d84eed22d14",
+    "url": "/reactive-todo/static/js/2.645af1ff.chunk.js"
+  },
+  {
+    "revision": "6cf2e2971caa2795c4d1",
+    "url": "/reactive-todo/static/js/main.2bc9ac2a.chunk.js"
+  },
+  {
+    "revision": "0d149e37ec8c5f9d24dc",
+    "url": "/reactive-todo/static/js/runtime-main.979fabdc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactive-todo/static/media/logo.25bf045c.svg"
+  }
+]);
